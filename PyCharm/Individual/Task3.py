e58c9ae163b9010aac3149521aa8a07ed714d049
BorsukovVladislav <borsukovv@inbox.ru>
2022-11-26 12:37:44 +0300

    fix Task3.py

--- PyCharm/Individual/Task3.py
+++ PyCharm/Individual/Task3.py
@@ -5,10 +5,10 @@ import math
 
 if __name__ == '__main__':
 
-    for N in range(100, 1000):
-        num1 = N // 100
-        num2 = (N % 100) // 10
-        num3 = N % 10
+    for n in range(100, 1000):
+        num1 = n // 100
+        num2 = (n % 100) // 10
+        num3 = n % 10
 
         if num1 + num2 + num3 == num1 * num2 * num3:
-            print(N)
+            print(n)
